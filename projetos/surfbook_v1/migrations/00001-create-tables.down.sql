72da6575b7b8040fe9b13be593e7b545a1e090e9
YagoSchramm <yrtoiu1515@gmail.com>
2026-04-01 14:28:19 -0300

    chore(1):New project chore

---
+++ projetos/surfbook_v1/migrations/00001-create-tables.down.sql
@@ -0,0 +1,16 @@
+```sql
+ 
+ DROP TABLE users;
+
+ DROP TABLE notebooks;
+
+ DROP TABLE meta_contents ;
+ 
+ DROP TABLE meta_tag_contents ;
+ 
+
+  DROP TABLE tags ;
+  
+
+
+ DROP TABLE nodes_contents;
