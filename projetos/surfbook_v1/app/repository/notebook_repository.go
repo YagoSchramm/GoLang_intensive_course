651a01c009abdcfd028456c45308842405a5e3f0
YagoSchramm <yrtoiu1515@gmail.com>
2026-04-02 17:42:44 -0300

    feature(8):Create notebook on repository

--- projetos/surfbook_v1/app/repository/notebook_repository.go
+++ projetos/surfbook_v1/app/repository/notebook_repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/YagoSchramm/intensivo-surfbook_v1/model"
+)
 
 type NotebookRepository struct {
 	db *sql.DB
@@ -8,4 +13,30 @@ type NotebookRepository struct {
 
 func NewNotebookRepository(d *sql.DB) *NotebookRepository {
 	return &NotebookRepository{db: d}
+}
+
+var listNotebookQuery string
+
+var createNotebookQuery string
+
+var deleteNotebookQuery string
+
+var updateNotebookQuery string
+
+var findByUserIDAndIDNotebookQuery string
+
+func (r *NotebookRepository) Create(ctx context.Context, notebook *model.NotebookEntity) error {
+	_, err := r.db.Exec(
+		createNotebookQuery,
+		notebook.NotebookID,
+		notebook.UserID,
+		notebook.Icon,
+		notebook.Name,
+		notebook.Description,
+		notebook.Image,
+		notebook.CreatedAt,
+		notebook.UpdatedAt,
+		notebook.DeletedAt,
+	)
+	return err
 }
